24647e84e733bef4cf2b803d398099be01c7e284
Paweł Kowalski <pavelloz@gmail.com>
2018-12-22 03:00:51 +0100

    Spoof useragent. Cleanup report

--- check-links.js
+++ check-links.js
@@ -10,47 +10,39 @@ const options = {
   honorRobotExclusions: false,
   // rateLimit: 100, // default: 0
   excludedKeywords: ['*api-reference/*', 'localhost'], // there is too much noise in there
-  maxSockets: 20
+  maxSockets: 20,
+  userAgent:
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 OPR/56.0.3051.116'
 };
 
 const customData = {
   failed: [],
-  succeeded: [],
-  index: 1
+  succeeded: []
 };
 
 const siteChecker = new blc.SiteChecker(options, {
   link: function(result, customData) {
     if (result.broken) {
+      console.log(
+        error(
+          `[BROKEN] Link: ${result.url.resolved} \
+          \n\t Page: ${result.base.resolved} \
+          \n\t Reason: ${blc[result.brokenReason]}`
+        )
+      );
       customData.failed.push(result);
     } else {
       customData.succeeded.push(result);
     }
-
-    if (!result.http.cached) {
-      const isOK = result.broken ? error('[BROKEN]') : success('[OK]');
-
-      console.log(`[${customData.index}]\t ${isOK} ${result.url.resolved}`);
-      customData.index = customData.index + 1;
-    }
-  },
-  page: function(error, pageUrl) {
-    console.log(`Finished page: ${pageUrl}`);
   },
   end: function() {
+    console.log('');
     if (customData.failed.length === 0) {
       console.log(success(`All links are working correctly at ${siteUrl}`));
       process.exit(0);
     } else {
-      console.log(success(`Number of correctly working links: ${customData.succeeded.length}`));
-
-      console.log(error(`Broken links (${customData.failed.length}):`));
-
-      customData.failed.forEach(fail => {
-        console.log(error(`\t${blc[fail.brokenReason]} :: ${fail.url.resolved}`));
-      });
-
-      console.log(`Checked links in total: ${customData.index}`);
+      console.log(success(`Correct: ${customData.succeeded.length}`));
+      console.log(error(`Broken: ${customData.failed.length}`));
 
       process.exit(1);
     }
