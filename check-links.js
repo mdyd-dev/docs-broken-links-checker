25551779b48523ee8ffce0b3136493f26dd655e2
Paweł Kowalski <pavelloz@gmail.com>
2019-12-21 15:51:25 +0100

    Tune down maxSocketsPerHost

--- check-links.js
+++ check-links.js
@@ -10,8 +10,7 @@ const options = {
   filterLevel: 0,
   honorRobotExclusions: false,
   excludedKeywords: ['*tablesgenerator.com*'],
-  // rateLimit: 20,
-  maxSocketsPerHost: 50,
+  maxSocketsPerHost: 30,
   userAgent:
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 };
