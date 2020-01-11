77eeaeb6e232cd57f936c1a640ee27f56d2a3b0c
Paweł Kowalski <pavelloz@gmail.com>
2020-01-11 19:32:00 +0100

    Slow down cowboy

--- check-links.js
+++ check-links.js
@@ -10,8 +10,8 @@ const options = {
   filterLevel: 0,
   honorRobotExclusions: false,
   excludedKeywords: ['*tablesgenerator.com*'],
-  rateLimit: 30,
-  maxSocketsPerHost: 20,
+  rateLimit: 50,
+  maxSocketsPerHost: 15,
   userAgent:
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 };
