6468892c26c015f0cec73aea50538fa2cc031742
Paweł Kowalski <pavelloz@gmail.com>
2020-04-29 14:27:34 +0200

    Try to not report test links

--- check-links.js
+++ check-links.js
@@ -10,7 +10,8 @@ const options = {
   filterLevel: 0,
   honorRobotExclusions: false,
   excludeExternalLinks: true,
-  rateLimit: 50,
+  excludedKeywords: ['*spec.html'],
+  rateLimit: 30,
   maxSocketsPerHost: 15,
   userAgent:
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
